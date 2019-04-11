5be46f90eada1ab852313be636d234587daec20f
jtloong <joshua.t.loong@gmail.com>
2019-04-11 16:19:55 -0400

    Added pushbullet functionality

--- retrieve.py
+++ retrieve.py
@@ -1,10 +1,12 @@
 import requests, json, random
+from pushbullet import Pushbullet
 
 
 with open('keys.json') as f:
     d = json.load(f)
 
-key = d['airtable']
+airtable_key = d['airtable']
+pushbullet_key = d['pushbullet']
 
 def select_article(key):
 	link = "https://api.airtable.com/v0/appdpiET2lgP7D2fM/Future%20Reads?view=Grid%20view"
@@ -28,3 +30,13 @@ def select_article(key):
 	selection = random.choice(range(len(titles)))
 
 	return titles[selection], links[selection]
+
+
+article_title, article_link = select_article(airtable_key)
+
+print(article_title)
+print(article_link)
+
+pb = Pushbullet(pushbullet_key)
+
+push = pb.push_link(article_title, article_link)
